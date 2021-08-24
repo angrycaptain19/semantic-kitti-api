741131f1e1e3e99b0e9570bad2bfb26ccb114794
nicoduchene <nduchene@sama.com>
2021-08-23 22:15:23 -0400

    Option to choose between file and buffer

--- auxiliary/laserscan.py
+++ auxiliary/laserscan.py
@@ -54,23 +54,27 @@ class LaserScan:
   def __len__(self):
     return self.size()
 
-  def open_scan(self, filename):
+  def open_scan(self, filename, file=False):
     """ Open raw scan and fill in attributes
     """
     # reset just in case there was an open structure
     self.reset()
 
-    # # check filename is string
-    # if not isinstance(filename, str):
-    #   raise TypeError("Filename should be string type, "
-    #                   "but was {type}".format(type=str(type(filename))))
+    if file:
+      # # check filename is string
+      if not isinstance(filename, str):
+        raise TypeError("Filename should be string type, "
+                        "but was {type}".format(type=str(type(filename))))
 
-    # # check extension is a laserscan
-    # if not any(filename.endswith(ext) for ext in self.EXTENSIONS_SCAN):
-    #   raise RuntimeError("Filename extension is not valid scan file.")
+      # # check extension is a laserscan
+      if not any(filename.endswith(ext) for ext in self.EXTENSIONS_SCAN):
+        raise RuntimeError("Filename extension is not valid scan file.")
 
-    # if all goes well, open pointcloud
-    scan = np.frombuffer(filename, dtype=np.float32)
+      # if all goes well, open pointcloud
+      scan = np.fromfile(filename, dtype=np.float32)
+    else:
+      scan = np.frombuffer(filename, dtype=np.float32)
+
     scan = scan.reshape((-1, 4))
 
     # put in attribute
@@ -216,20 +220,24 @@ class SemLaserScan(LaserScan):
     self.proj_inst_color = np.zeros((self.proj_H, self.proj_W, 3),
                                     dtype=np.float)              # [H,W,3] color
 
-  def open_label(self, filename):
+  def open_label(self, filename, file=False):
     """ Open raw scan and fill in attributes
     """
-    # # check filename is string
-    # if not isinstance(filename, str):
-    #   raise TypeError("Filename should be string type, "
-    #                   "but was {type}".format(type=str(type(filename))))
+    if file:
+      # # check filename is string
+      if not isinstance(filename, str):
+        raise TypeError("Filename should be string type, "
+                        "but was {type}".format(type=str(type(filename))))
 
-    # # check extension is a laserscan
-    # if not any(filename.endswith(ext) for ext in self.EXTENSIONS_LABEL):
-    #   raise RuntimeError("Filename extension is not valid label file.")
+      # # check extension is a laserscan
+      if not any(filename.endswith(ext) for ext in self.EXTENSIONS_LABEL):
+        raise RuntimeError("Filename extension is not valid label file.")
 
-    # if all goes well, open label
-    label = np.frombuffer(filename, dtype=np.uint32)
+      # if all goes well, open label
+      label = np.fromfile(filename, dtype=np.uint32)
+    else:
+      label = np.frombuffer(filename, dtype=np.uint32)
+
     label = label.reshape((-1))
 
     # set it
